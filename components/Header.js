ff6c9640f9be68802e59de90e0b53ebe5b591676
telmo bidart <telmobidart@gmail.com>
2022-01-13 12:55:26 -0300

    feat(header) added header style

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,18 @@
 import Image from "next/image";
 
-function Header () {
-    return (
-      <header>
-          <h1>
-              this is the header
-          </h1>
-          <Image src='https://links.papareact.com/ua6' widht={200} height={100} alt='logo' />
-      </header>
-    )
+function Header() {
+  return (
+    <header>
+      <h1>this is the header</h1>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+        alt="logo"
+      />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
