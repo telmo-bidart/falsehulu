0a6ebb42c19950ee71dd56f5b84981b488adca20
telmo bidart <telmobidart@gmail.com>
2022-01-17 17:57:23 -0300

    final build

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,6 +1,7 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 
+
 function Thumbnail({ result }) {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
